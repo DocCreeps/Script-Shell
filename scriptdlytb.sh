a2ba24cd5834b6ba91425ff6112ffbada967bf57
Dorian <33058222+DocCreeps@users.noreply.github.com>
2021-05-01 15:24:56 +0200

    Update scriptdlytb.sh

--- scriptdlytb.sh
+++ scriptdlytb.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 echo "Vidéo downloader by Doccreeps v1"
-read -p "Entrez l'URL de la playlist ou  de la vidéo a télécharger  :" urlytb
-echo "votre playlist/vidéo va être télécharger"
+read -p "Entrez l'URL de la playlist ou de la vidéo à télécharger  :" urlytb
+echo "Votre playlist/vidéo va être télécharger"
 cd ../storage/youtubedl
 youtube-dl --extract-audio --audio-format mp3 $urlytb
-cp *.mp3 ../storage/music/youtubedl
+cp *.mp3 ../music/youtubedl
 
